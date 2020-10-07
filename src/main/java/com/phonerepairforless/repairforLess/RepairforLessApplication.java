90b14e0db0c11ec2ef272d7b4c924644e9b3c3a5
Dipendra Pokharel <dipen@Dipendras-MBP.tx.rr.com>
2020-10-06 19:23:37 -0500

    First Project Starter

---
+++ src/main/java/com/phonerepairforless/repairforLess/RepairforLessApplication.java
@@ -0,0 +1,13 @@
+package com.phonerepairforless.repairforLess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairforLessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairforLessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phonerepairforless/repairforLess/RepairforLessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phonerepairforless.repairforLess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairforLessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
